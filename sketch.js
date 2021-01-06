662f1d21890f07ff6730abb22d5489a2a16116cf
SteFxcLah <70620802+StigneiStefan@users.noreply.github.com>
2021-01-05 20:48:14 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var a
+function setup() {
+    createCanvas(400, 400).parent('between');
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
